a7ae1df19dfd6e3d90ebbbc1ed60e90b4cfdc37c
Prakashdhanasek <prakash@ngxptechnologies.com>
2026-07-08 10:36:17 +0530

    changes in build upload

--- android/app/src/main/kotlin/com/example/monitoring_driver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/monitoring_driver/MainActivity.kt
@@ -199,11 +199,16 @@ class MainActivity : FlutterActivity() {
             }
 
             val callbackIntent = Intent(action).setPackage(packageName)
+            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            } else {
+                PendingIntent.FLAG_UPDATE_CURRENT
+            }
             val pendingIntent = PendingIntent.getBroadcast(
                 applicationContext,
                 sessionId,
                 callbackIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                flags
             )
 
             session.commit(pendingIntent.intentSender)
